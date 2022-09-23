9a94c46d22e83e0ae539341557849c4383b902e3
“MaXqIvanov” <maksivanov35@ya.ru>
2022-09-23 17:57:40 +0300

    update

--- src/components/tasks/TableHeader.tsx
+++ src/components/tasks/TableHeader.tsx
@@ -7,17 +7,20 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import footer_left_btn from '../../assets/task/footer_left_btn.svg'
+import footer_right_btn from '../../assets/task/footer_right_btn.svg'
 
-function createData(number: number, name_task:string, begining:number, OA:any, carbs:number, protein:number) {
-    return {number, name_task, begining, OA, carbs, protein };
+function createData(number: number, name_task:string, begining:number, OA:any, carbs:number, monday:number,
+    thuesday: number, thirtday: number, firstday: number, friday: number, saturday: number, sunday: number) {
+    return {number, name_task, begining, OA, carbs, monday, thuesday, thirtday, firstday, friday, saturday, sunday};
   }
   
   const rows = [
-    createData(1, 'Frozen yoghurt', 159, "file", 24, 4.0),
-    createData(2, 'Ice cream sandwich', 237, "file", 37, 4.3),
-    createData(3, 'Eclair', 262, "file", 24, 6.0),
-    createData(4, 'Cupcake', 305, "file", 67, 4.3),
-    createData(5, 'Gingerbread', 356, "file", 49, 3.9),
+    createData(1, 'Frozen yoghurt', 159, "file", 24, 4.0, 10, 20, 30, 40, 50, 60),
+    createData(2, 'Ice cream sandwich', 237, "file", 37, 4.3, 10, 20, 30, 40, 50, 60),
+    createData(3, 'Eclair', 262, "file", 24, 6.0 , 10, 20, 30, 40, 50, 60),
+    createData(4, 'Cupcake', 305, "file", 67, 4.3, 10, 20, 30, 40, 50, 60),
+    createData(5, 'Gingerbread', 356, "file", 49, 3.9, 10, 20, 30, 40, 50, 60),
   ];
 
 export const TableHeader = () => {
@@ -34,6 +37,12 @@ export const TableHeader = () => {
                         <TableCell align="right">Норма</TableCell>
                         <TableCell align="right">О/А</TableCell>
                         <TableCell align="right">ПН, 12 сен 4.0</TableCell>
+                        <TableCell align="right">ВН, 13 сен 5.0</TableCell>
+                        <TableCell align="right">СР, 14 сен 6.0</TableCell>
+                        <TableCell align="right">ЧТ, 15 сен 7.0</TableCell>
+                        <TableCell align="right">ПТ, 16 сен 8.0</TableCell>
+                        <TableCell align="right">СБ, 17 сен 7.0</TableCell>
+                        <TableCell align="right">ВС, 18 сен 8.0</TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
@@ -51,13 +60,25 @@ export const TableHeader = () => {
                         <TableCell align="right">{row.begining}</TableCell>
                         <TableCell align="right">{row.OA}</TableCell>
                         <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
+                        <TableCell align="right">{row.monday}</TableCell>
+                        <TableCell align="right">{row.thuesday}</TableCell>
+                        <TableCell align="right">{row.thirtday}</TableCell>
+                        <TableCell align="right">{row.firstday}</TableCell>
+                        <TableCell align="right">{row.friday}</TableCell>
+                        <TableCell align="right">{row.saturday}</TableCell>
+                        <TableCell align="right">{row.sunday}</TableCell>
                         </TableRow>
                     ))}
                     </TableBody>
                 </Table>
                 <div className={styles.thead_footer_custom}>
-                    
+                    <div>страница</div>
+                    <div className={styles.footer_group_btn}>
+                        <div style={{backgroundImage: `url(${footer_left_btn})`}} className={styles.footer_group_btn_left}></div>
+                        <div className={styles.footer_group_btn_center}></div>
+                        <div style={{backgroundImage: `url(${footer_right_btn})`}} className={styles.footer_group_btn_right}></div>
+                    </div>
+                    <div>из 2</div>
                 </div>
             </TableContainer>
         </div>
