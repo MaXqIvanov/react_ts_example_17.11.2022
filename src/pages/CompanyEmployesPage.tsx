9829f7c95bb7f6abc6285ee5dc60f51660824efe
“MaXqIvanov” <maksivanov35@ya.ru>
2022-09-26 15:40:33 +0300

    update

--- src/pages/CompanyEmployesPage.tsx
+++ src/pages/CompanyEmployesPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { NavHeaderComEmployes } from '../components/company_employes/NavHeaderComEmployes'
+import { TableHeaderComEmployes } from '../components/company_employes/TableHeaderComEmployes'
+import styles from '../scss/CompanyEmployes.module.scss'
 
 export const CompanyEmployesPage = () => {
   return (
-    <div>CompanyEmployesPage</div>
+    <div className={styles.company_employes}>
+      <div className={styles.company_employes_wrapper}>
+        <NavHeaderComEmployes />
+        <TableHeaderComEmployes />
+      </div>
+  </div>
   )
 }
