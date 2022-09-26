ad1a68643690b540f7698160ea4566a6b7356405
“MaXqIvanov” <maksivanov35@ya.ru>
2022-09-26 17:38:49 +0300

    update

--- src/pages/CompanyPositionsPage.tsx
+++ src/pages/CompanyPositionsPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { NavHeaderComPosition } from '../components/company_positions/NavHeaderComPosition'
+import { TableHeaderComPosition } from '../components/company_positions/TableHeaderComPosition'
+import styles from '../scss/CompanyPosition.module.scss'
 
 export const CompanyPositionsPage = () => {
   return (
-    <div>CompanyPositionsPage</div>
+    <div className={styles.company_employes}>
+      <div className={styles.company_employes_wrapper}>
+        <NavHeaderComPosition />
+        <TableHeaderComPosition />
+      </div>
+  </div>
   )
 }
